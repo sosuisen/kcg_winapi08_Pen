820aa46c24dce7c76922ed5a8ef945f035634f20
sosuisen <hidekazu.kubota@gmail.com>
2022-11-08 12:35:11 +0900

    fix: remove WM_CLOSE

--- Pen/Pen.cpp
+++ Pen/Pen.cpp
@@ -2,8 +2,8 @@
 #include <windowsx.h> // GET_X_LPARAN, GET_Y_LPARAMマクロの定義
 #include <string>
 
-static const int MAX_WIDTH = 800;
-static const int MAX_HEIGHT = 600;
+static const int WIN_WIDTH = 800;
+static const int WIN_HEIGHT = 600;
 
 LRESULT CALLBACK WndProc(
     HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -11,7 +11,7 @@ LRESULT CALLBACK WndProc(
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
-    TCHAR szAppName[] = L"PaintApp";
+    TCHAR szAppName[] = L"PenApp";
     WNDCLASS wc;
     HWND hwnd;
     MSG msg;
@@ -36,7 +36,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         szAppName, L"Paint",
         WS_OVERLAPPEDWINDOW,
         50, 50,
-        MAX_WIDTH, MAX_HEIGHT,
+        WIN_WIDTH, WIN_HEIGHT,
         NULL, NULL,
         hInstance, NULL);
 
@@ -58,20 +58,35 @@ LRESULT CALLBACK WndProc(
 {
     static HBITMAP  hBitmap;    // ビットマップ
     static HDC      hMemDC;     // オフスクリーン
+    static HPEN     hpen;
+    static HBRUSH   hbr;
 
     HDC hdc;
 
     switch (uMsg) {
-    case WM_CREATE:
+    case WM_CREATE: {
         // オフスクリーンをメモリデバイスコンテキストを用いて作成
         hdc = GetDC(hwnd);
         hMemDC = CreateCompatibleDC(hdc);
-        hBitmap = CreateCompatibleBitmap(hdc, MAX_WIDTH, MAX_HEIGHT);
+        hBitmap = CreateCompatibleBitmap(hdc, WIN_WIDTH, WIN_HEIGHT);
         SelectObject(hMemDC, hBitmap);
         // オフスクリーンを白で塗りつぶす（デフォルトは黒）
-        PatBlt(hMemDC, 0, 0, MAX_WIDTH, MAX_HEIGHT, WHITENESS);
+        PatBlt(hMemDC, 0, 0, WIN_WIDTH, WIN_HEIGHT, WHITENESS);
+
+        // ペンとブラシを作成
+        hpen = CreatePen(PS_SOLID, 5, RGB(0x00, 0x7f, 0xff));
+        hbr = CreateSolidBrush(RGB(0xff, 0xbf, 0x00));
+
+        // ペンとブラシを選択
+        SelectObject(hMemDC, hpen);
+        SelectObject(hMemDC, hbr);
+
+        // オフスクリーンへ描画
+        Rectangle(hMemDC, 50, 50, 200, 150);
+
         ReleaseDC(hwnd, hdc);
         return 0;
+    }
     case WM_CLOSE:
         // オフスクリーンの破棄
         DeleteDC(hMemDC);
@@ -81,33 +96,18 @@ LRESULT CALLBACK WndProc(
     case WM_PAINT:
         PAINTSTRUCT paint;
         hdc = BeginPaint(hwnd, &paint);
-
         // ビットブロック転送
         BitBlt(
             hdc,
             0,
             0,
-            WINDOW_WIDTH,
-            WINDOW_HEIGHT,
+            WIN_WIDTH,
+            WIN_HEIGHT,
             hMemDC,
-            paint.rcPaint.left,
-            paint.rcPaint.top,
+            0,
+            0,
             SRCCOPY);
-
-        // 更新領域の左上座標と幅、高さを表示
-        // x, y, width, height
-        /*
-        swprintf_s(buf, 18, format,
-            paint.rcPaint.left,
-            paint.rcPaint.top,
-            paint.rcPaint.right - paint.rcPaint.left,
-            paint.rcPaint.bottom - paint.rcPaint.top);
-        OutputDebugString(buf);
-        */
-
-
         EndPaint(hwnd, &paint);
-
         return 0;
     case WM_DESTROY:
         PostQuitMessage(0);
